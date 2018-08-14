2319a26ba82a70b673dd0ad448d19c22996fe05e
Georgete Bueno <geobueno@gmail.com>
2018-08-14 10:35:25 -0300

    Funcao edid pronta

--- tasks.js
+++ tasks.js
@@ -4,11 +4,11 @@ var userID = window.location.search.match(/\?id=(.*)/)[1];
 $(document).ready(function() {
     getTasksFromDB();
     $(".add-tasks").click(addTasksClick);
+
 });
 
 function addTasksClick(event) {
     event.preventDefault();
-
     var newTask = $(".tasks-input").val();
     var taskFromDB = addTaskToDB(newTask);
     crudListItem(newTask, taskFromDB.key)
@@ -49,16 +49,21 @@ function crudListItem(text, key) {
         $(this).parent().remove();
         $(".tasks-list").append(`
         <li>
-        <textarea>${text}</textarea>
-          <button class="delete" data-task-id=${key}>Deletar</button>
-          <button class="edit" data-task-id=${key}>Update</button>
+        <textarea class="tasks-update">${text}</textarea>
+          <button class="update-tasks">Update</button>
         </li>`);
-
-
-
-
+        $(".update-tasks").click(updateTasksClick);
 
 
     });
+}
+
+function updateTasksClick(event) {
+    event.preventDefault();
+    var newTask = $(".tasks-update").val();
+    var taskFromDB = addTaskToDB(newTask);
+    crudListItem(newTask, taskFromDB.key);
+    $(".tasks-update").parent().remove();
+    $(".update-tasks").parent().remove();
 
 }
